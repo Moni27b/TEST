497577fb46c05f6d76af902d94afcf41995a4139
Moni27b <moni27b@hotmail.com>
2017-02-01 11:15:14 +0100

    Home Pay version 1.3
    Updating database

--- BDD_HomePay.sql
+++ BDD_HomePay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 27, 2017 at 11:37 AM
+-- Generation Time: Feb 01, 2017 at 11:14 AM
 -- Server version: 5.6.34
 -- PHP Version: 7.1.0
 
@@ -11,132 +11,92 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `HomePay`
+-- Database: `PFE_Tracker`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CB`
+-- Table structure for table `Mesure`
 --
 
-CREATE TABLE `CB` (
-  `id` int(11) NOT NULL,
-  `Num_CB` varchar(16) NOT NULL,
-  `Mois` varchar(255) NOT NULL,
-  `Annee` varchar(255) NOT NULL,
-  `id_client` int(11) NOT NULL,
-  `Solde` int(11) NOT NULL
+CREATE TABLE `Mesure` (
+  `Id_Mesure` int(11) NOT NULL,
+  `Date_Mesure` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Adresse_Routeur` varchar(255) NOT NULL,
+  `Mode` varchar(20) NOT NULL,
+  `Id_Tracker` int(11) NOT NULL,
+  `Id_Routeur` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
 
 --
--- Dumping data for table `CB`
+-- Dumping data for table `Mesure`
 --
 
-INSERT INTO `CB` (`id`, `Num_CB`, `Mois`, `Annee`, `id_client`, `Solde`) VALUES
-(8, '1111222233334444', 'Avril', '2018', 8, 100),
-(10, '9999888877776666', 'Avril', '2019', 8, 50);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Client`
---
-
-CREATE TABLE `Client` (
-  `id` int(11) NOT NULL,
-  `N_Compte` int(11) NOT NULL,
-  `Nom` varchar(255) NOT NULL,
-  `Adresse` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
-
---
--- Dumping data for table `Client`
---
-
-INSERT INTO `Client` (`id`, `N_Compte`, `Nom`, `Adresse`, `Password`) VALUES
-(8, 123456, 'Jack', 'Cholet', 'JackJack'),
-(9, 121234, 'Cecilia', 'Cherbourg', 'CeciliaCecilia');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Paiment`
---
-
-CREATE TABLE `Paiment` (
-  `idPaiment` int(11) NOT NULL,
-  `Commercant` varchar(255) NOT NULL,
-  `Montant` int(11) NOT NULL,
-  `idCB` int(11) NOT NULL,
-  `DateP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
-
---
--- Dumping data for table `Paiment`
---
-
-INSERT INTO `Paiment` (`idPaiment`, `Commercant`, `Montant`, `idCB`, `DateP`) VALUES
-(1, 'Auchan', 14, 8, '2017-01-27 10:05:01');
+INSERT INTO `Mesure` (`Id_Mesure`, `Date_Mesure`, `Adresse_Routeur`, `Mode`, `Id_Tracker`, `Id_Routeur`) VALUES
+(11, '2016-12-08 15:36:01', '77:77:77:77', 'ON', 2, 5),
+(12, '2016-12-08 16:58:29', '77:77:77:77', 'OFF', 2, 5),
+(17, '2016-12-13 18:29:34', '66:66:66:66', 'ON', 1, 4),
+(18, '2016-12-13 20:44:55', '66:66:66:66', 'OFF', 1, 4),
+(24, '2017-01-27 14:07:06', '55:55:55:55', 'ON', 1, 3),
+(25, '2017-01-27 15:45:09', '55:55:55:55', 'OFF', 1, 3),
+(26, '2017-01-28 12:51:52', '33:33:33:33:33', 'ON', 3, 1),
+(27, '2017-01-28 12:53:15', '44:44:44:44', 'ON', 2, 2),
+(28, '2017-01-28 12:53:50', '44:44:44:44', 'ON', 1, 2),
+(29, '2017-01-28 16:26:51', '33:33:33:33:33', 'OFF', 3, 1),
+(30, '2017-01-28 17:33:09', '44:44:44:44', 'OFF', 2, 2),
+(31, '2017-01-28 18:49:12', '44:44:44:44', 'OFF', 1, 2),
+(32, '2017-01-29 12:06:11', '33:33:33:33:33', 'ON', 3, 1),
+(33, '2017-01-29 16:17:16', '44:44:44:44', 'ON', 2, 2),
+(34, '2017-01-29 16:17:32', '33:33:33:33:33', 'OFF', 3, 1),
+(35, '2017-01-29 16:17:55', '44:44:44:44', 'ON', 1, 2),
+(38, '2017-01-29 17:44:10', '44:44:44:44', 'OFF', 2, 2),
+(39, '2017-01-29 22:00:00', '44:44:44:44', 'OFF', 1, 2),
+(40, '2017-01-30 14:29:02', '33:33:33:33:33', 'ON', 3, 1),
+(41, '2017-01-30 19:55:59', '33:33:33:33:33', 'OFF', 3, 1),
+(42, '2016-12-13 07:00:00', '55:55:55:55', 'ON', 3, 3),
+(43, '2016-12-13 18:00:00', '55:55:55:55', 'OFF', 3, 3),
+(44, '2017-01-10 06:11:00', '66:66:66:66', 'ON', 2, 4),
+(45, '2017-01-10 17:39:00', '66:66:66:66', 'OFF', 2, 4),
+(46, '2017-01-03 09:13:00', '77:77:77:77', 'ON', 3, 5),
+(47, '2017-01-03 15:20:00', '77:77:77:77', 'OFF', 3, 5),
+(48, '2017-01-03 16:11:00', '44:44:44:44', 'ON', 3, 2),
+(50, '2017-01-03 18:08:00', '44:44:44:44', 'OFF', 3, 2),
+(51, '2017-01-30 08:19:12', '77:77:77:77', 'ON', 2, 5),
+(52, '2017-01-30 12:38:25', '77:77:77:77', 'OFF', 2, 5),
+(53, '2016-12-20 09:00:00', '55:55:55:55', 'ON', 5, 3),
+(54, '2016-12-20 15:00:00', '55:55:55:55', 'OFF', 5, 3),
+(55, '2017-01-05 07:00:00', '33:33:33:33:33', 'ON', 5, 1),
+(56, '2017-01-05 13:19:00', '33:33:33:33:33', 'OFF', 5, 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `CB`
+-- Indexes for table `Mesure`
 --
-ALTER TABLE `CB`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `Num_CB` (`Num_CB`),
-  ADD KEY `id_client` (`id_client`),
-  ADD KEY `Num_CB_2` (`Num_CB`);
-
---
--- Indexes for table `Client`
---
-ALTER TABLE `Client`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `Paiment`
---
-ALTER TABLE `Paiment`
-  ADD PRIMARY KEY (`idPaiment`),
-  ADD KEY `idCB` (`idCB`);
+ALTER TABLE `Mesure`
+  ADD PRIMARY KEY (`Id_Mesure`),
+  ADD KEY `Id_Tracker` (`Id_Tracker`),
+  ADD KEY `Id_Routeur` (`Id_Routeur`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `CB`
+-- AUTO_INCREMENT for table `Mesure`
 --
-ALTER TABLE `CB`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT for table `Client`
---
-ALTER TABLE `Client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `Paiment`
---
-ALTER TABLE `Paiment`
-  MODIFY `idPaiment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `Mesure`
+  MODIFY `Id_Mesure` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `CB`
+-- Constraints for table `Mesure`
 --
-ALTER TABLE `CB`
-  ADD CONSTRAINT `cb_ibfk_2` FOREIGN KEY (`id_client`) REFERENCES `Client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `Paiment`
---
-ALTER TABLE `Paiment`
-  ADD CONSTRAINT `paiment_ibfk_1` FOREIGN KEY (`idCB`) REFERENCES `CB` (`id`);
+ALTER TABLE `Mesure`
+  ADD CONSTRAINT `mesure_ibfk_1` FOREIGN KEY (`Id_Tracker`) REFERENCES `Tracker` (`Id_Tracker`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesure_ibfk_2` FOREIGN KEY (`Id_Routeur`) REFERENCES `Routeur` (`Id_Routeur`) ON DELETE CASCADE ON UPDATE CASCADE;
